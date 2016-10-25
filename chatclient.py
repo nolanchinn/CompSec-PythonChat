adb421f99d48aabab29c328edea4b2c6d31e3cae
Nolan Chinn <nolan.chinn@wallawalla.edu>
2016-10-25 14:04:21 -0700

    refactor client code into its own class

--- chatclient.py
+++ chatclient.py
@@ -1,37 +1,41 @@
 import socket, select, string, sys
-serverIP = '0.0.0.0'
-serverPort = 5000
-name = 'generic guy'
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((serverIP, serverPort))
-print 'Connected to server. Start sending messages.'
-while(1):
-	"""inputReady, outputReady, exceptReady = select.select([sys.stdin, sock],[], [])
-	for inputs in inputReady:
-		message, address = inputs.recvfrom(4096)
-		print message
-		if(sys.stdin):
-			rawData = sys.stdin.readline()
-			if(rawData.startswith('exit')):
-				print '~~~EXITING~~~'
-				sys.exit()
-			else: 
-				sock.send(rawData)"""
-	socket_list = [sys.stdin, sock]
-	read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
 
-	for rSock in read_sockets:
-		if rSock == sock:
-			data, address = rSock.recvfrom(4096)
-			if not data :
-				print '\nDisconnected'
-				sys.exit()
+class Client: 
+
+	def __init__(self, ip, port, name):
+		self.serverIP = ip
+		self.serverPort = port
+		self.name = name
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	def connect(self):
+		self.sock.connect((self.serverIP, self.serverPort))
+		print 'Connected to the server.'
+
+	def sendRecvMsg(self, read_sockets):
+		for rSock in read_sockets:
+			if rSock == self.sock:
+				data, addr = rSock.recvfrom(4096)
+				if not data:
+					print '\nDisconnected from the server.'
+					sys.exit()
+				else:
+					sys.stdout.write(data)
 			else:
-				sys.stdout.write(data)
-		else :
-			msg = sys.stdin.readline()
-			sock.send(name + ': ' + msg)
-sock.close()
+				msg = sys.stdin.readline()
+				self.sock.send(msg)
+
+	def run(self):
+		while(1):
+			socket_list = [sys.stdin, self.sock]
+			read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
+			self.sendRecvMsg(read_sockets)
+		self.sock.close()
+
+if __name__ == "__main__":
+	client = Client('0.0.0.0', 5000, 'nolan')
+	client.connect()
+	client.run()
 
 	
 
